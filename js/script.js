c10b69563e400cd05fb2c57a83e3ca730f16f18a
Francesco-Rianna <maradonino98@hotmail.it>
2024-04-09 15:11:18 +0200

    feat : print mail in page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
+
+// Ciao ragazzi,
+// Esercizio di oggi: Vue Email List
+// nome repo: vue-email-list
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
+            myEmailArray: [],
+
 
         };
+
+
     },
     methods: {
+        getEmailFromApi() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                        const serverData = response.data;
+                        this.myEmailArray.push(serverData.response)
+                    })
+            }
+
+        }
+    },
+    mounted() {
+        this.getEmailFromApi()
 
     }
 }).mount('#app');
